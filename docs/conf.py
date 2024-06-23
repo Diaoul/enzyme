49e3b241e86230fd86f636021d8169b968bf163d
Antoine Bertin <antoine@bertin.io>
2024-06-23 14:34:16 +0200

    fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,7 @@
 # serve to show the default.
 
 import sys, os
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -95,26 +96,26 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "diaoul"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    "github_user": "Diaoul",
-    "github_repo": "enzyme",
-    "github_branch": "master",
-    "fork_me": 1,
-    "flattr": 0,
-    "gittip": "Diaoul",
-    "pypi_downloads": 1,
-    "pypi_version": 0,
-    "travis": 0,
-    "coveralls": 0,
-}
+# html_theme_options = {
+#     "github_user": "Diaoul",
+#     "github_repo": "enzyme",
+#     "github_branch": "master",
+#     "fork_me": 1,
+#     "flattr": 0,
+#     "gittip": "Diaoul",
+#     "pypi_downloads": 1,
+#     "pypi_version": 0,
+#     "travis": 0,
+#     "coveralls": 0,
+# }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["_themes"]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
